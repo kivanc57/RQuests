60bb64ef020beb13efdcd660d7468a144f89fc61
Kivanc Gordu <kivancgordu@hotmail.com>
2024-10-29 16:56:23 +0100

    Add quest_10.R

---
+++ quests/quest_10.R
@@ -0,0 +1,36 @@
+# Quest 10
+# Find if "i" or "y" is more frequent in a Czech text
+
+# Get the remote built-int functions
+functions <- source("https://raw.githubusercontent.com/oltkkol/vmod/master/simplest_text.R")
+
+# Get the content
+content <- GetFileContent("../data/test.txt")
+
+# Tokenize text with regex
+results <- TokenizeText(content, "[iíyý]", regexIsMask=TRUE)
+
+# Get results
+table(results)
+
+# Relative difference constant
+relative_constant <- 123
+
+# Create difference_table vector
+difference_table <- numeric(1000)
+
+# Randomize the results
+for (i in 1:1000){
+  randomized_results <- sample(results, relative_constant, replace=TRUE)
+  results_i <- length(which(randomized_results == "i"))
+  results_í <- length(which(randomized_results == "í"))
+  results_y <- length(which(randomized_results == "y"))
+  results_ý <- length(which(randomized_results == "ý"))
+  
+  difference <- abs(((results_i + results_í) - (results_y + results_ý)) / (relative_constant))
+  difference_table[i] <- difference
+  hist(difference_table)
+}
+
+# Get confidence interval
+qnorm( c(0.025, 0.975), mean(difference_table), sd(difference_table))
